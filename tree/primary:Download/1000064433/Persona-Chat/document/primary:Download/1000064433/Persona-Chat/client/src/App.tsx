0c3d11cb5c48402f7402cae6cd91442b97078453
zizozizo123km <nnacar12@gmail.com>
2026-02-07 12:12:02 +0100

    Upload tree/primary%3ADownload%2F1000064433%2FPersona-Chat/document/primary%3ADownload%2F1000064433%2FPersona-Chat%2Fclient%2Fsrc%2FApp.tsx via Auto-Uploader

---
+++ tree/primary:Download/1000064433/Persona-Chat/document/primary:Download/1000064433/Persona-Chat/client/src/App.tsx
@@ -0,0 +1,36 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Layout } from "@/components/layout/Layout";
+import Home from "@/pages/Home";
+import Chat from "@/pages/Chat";
+import Characters from "@/pages/Characters";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/characters" component={Characters} />
+      <Route path="/chat/:id" component={Chat} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Layout>
+          <Router />
+        </Layout>
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
